88fcd3344df64203434a2089c07f47523dbe7071
Michal Kosinski <kosinski.michal@gmail.com>
2018-01-13 15:12:49 +0100

    added tensorboard hooks, updated learning rate, acheived convergence

--- squeezenet.py
+++ squeezenet.py
@@ -21,6 +21,11 @@ def squeeze(input, channels, layer_num):
         onebyone = tf.nn.conv2d(input, weights, strides=(1, 1, 1, 1), padding='VALID') + biases
         A = tf.nn.relu(onebyone)
 
+        tf.summary.histogram('weights', weights)
+        tf.summary.histogram('biases', biases)
+        tf.summary.histogram('logits', onebyone)
+        tf.summary.histogram('activations', A)
+
     return A
 
 # define expand module
@@ -34,7 +39,7 @@ def expand(input, channels_1by1, channels_3by3, layer_num):
     :return: output tensor convoluted with expand layer
     """
 
-    layer_name = 'squeeze_' + str(layer_num)
+    layer_name = 'expand_' + str(layer_num)
     input_channels = input.get_shape().as_list()[3]
 
     with tf.name_scope(layer_name):
@@ -43,10 +48,20 @@ def expand(input, channels_1by1, channels_3by3, layer_num):
         onebyone = tf.nn.conv2d(input, weights1x1, strides=(1, 1, 1, 1), padding='VALID') + biases1x1
         A_1x1 = tf.nn.relu(onebyone)
 
+        tf.summary.histogram('weights [1x1]', weights1x1)
+        tf.summary.histogram('biases [1x1]', biases1x1)
+        tf.summary.histogram('logits [1x1]', onebyone)
+        tf.summary.histogram('activations [1x1]', A_1x1)
+
         weights3x3 = tf.Variable(tf.contrib.layers.xavier_initializer()([1, 1, input_channels, channels_3by3]))
         biases3x3 = tf.Variable(tf.zeros([1, 1, 1, channels_3by3]), name='biases')
         threebythree = tf.nn.conv2d(input, weights3x3, strides=(1, 1, 1, 1), padding='SAME') + biases3x3
         A_3x3 = tf.nn.relu(threebythree)
+
+        tf.summary.histogram('weights [3x3]', weights3x3)
+        tf.summary.histogram('biases [3x3]', biases3x3)
+        tf.summary.histogram('logits [3x3]', threebythree)
+        tf.summary.histogram('activations [3x3]', A_3x3)
 
     return tf.concat([A_1x1, A_3x3], axis=3)
 
@@ -86,6 +101,7 @@ def model(input_height, input_width, input_channels, output_classes, pooling_siz
         in_training = tf.placeholder(tf.bool, shape=())
         learning_rate = tf.placeholder(tf.float32, shape=())
 
+        tf.summary.image('input image', input_image)
     # define structure of the net
     # layer 1 - conv 1
         with tf.name_scope('conv_1'):
@@ -93,9 +109,12 @@ def model(input_height, input_width, input_channels, output_classes, pooling_siz
             b_conv1 = tf.Variable(tf.zeros([1, 1, 1, 96]))
             X_1 = tf.nn.conv2d(input_image, W_conv1, strides=(1, 2, 2, 1), padding='VALID') + b_conv1
             A_1 = tf.nn.relu(X_1)
+            tf.summary.histogram('conv1 weights', W_conv1)
+            tf.summary.histogram('conv1 biases', b_conv1)
+            tf.summary.histogram('conv1 logits', X_1)
+            tf.summary.histogram('conv1 activations', A_1)
 
         # layer 2 - maxpool
-        # TODO: smaller size maxpool for small networks
         maxpool_1 = tf.nn.max_pool(A_1, ksize=pooling_size, strides=(1, 2, 2, 1), padding='VALID', name='maxpool_1')
 
         # layer 3-5 - fire modules
@@ -104,7 +123,6 @@ def model(input_height, input_width, input_channels, output_classes, pooling_siz
         fire_4 = fire_module(fire_3, 32, 128, 128, layer_num=4)
 
         # layer 6 - maxpool
-        # TODO: smaller size maxpool for small networks
         maxpool_4 = tf.nn.max_pool(fire_4, ksize=pooling_size, strides=(1, 2, 2, 1), padding='VALID', name='maxpool_4')
 
         # layer 7-10 - fire modules
@@ -114,7 +132,6 @@ def model(input_height, input_width, input_channels, output_classes, pooling_siz
         fire_8 = fire_module(fire_7, 64, 256, 256, layer_num=8)
 
         # layer 11 - maxpool
-        # TODO: smaller size maxpool for small networks
         maxpool_8 = tf.nn.max_pool(fire_8, ksize=pooling_size, strides=(1, 2, 2, 1), padding='VALID', name='maxpool_8')
 
         # layer 12 - fire 9 + dropout
@@ -127,27 +144,36 @@ def model(input_height, input_width, input_channels, output_classes, pooling_siz
         # layer 13 - final
         with tf.name_scope('final'):
             W_conv10 = tf.Variable(tf.contrib.layers.xavier_initializer()([1, 1, 512, output_classes]))
-            b_conv10 = tf.Variable(tf.zeros([]))
+            b_conv10 = tf.Variable(tf.zeros([1, 1, 1, 10]))
             conv_10 = tf.nn.conv2d(dropout_9, W_conv10, strides=(1, 1, 1,1), padding='VALID') + b_conv10
             A_conv_10 = tf.nn.relu(conv_10)
+
+            tf.summary.histogram('conv10 weights', W_conv10)
+            tf.summary.histogram('conv10 biases', b_conv10)
+            tf.summary.histogram('conv10 logits', conv_10)
+            tf.summary.histogram('conv10 activations', A_conv_10)
 
         # avg pooling to get [1 x 1 x num_classes] must average over entire window oh H x W from input layer
         _, H_last, W_last, _ = A_conv_10.get_shape().as_list()
         pooled = tf.nn.avg_pool(A_conv_10, ksize=(1, H_last, W_last, 1), strides=(1, 1, 1, 1), padding='VALID')
-        logits = tf.squeeze(pooled, axis=[2])
+        #logits = tf.squeeze(pooled, axis=[2])
 
         # loss + optimizer
         one_hot_labels = tf.one_hot(labels, output_classes)
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_labels, logits=logits))
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_labels, logits=pooled))
+        tf.summary.scalar('loss', loss)
         optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
         # accuracy
-        logits_class_num = tf.cast(tf.argmax(tf.nn.softmax(logits), axis=1), tf.int32)
+        logits_class_num = tf.cast(tf.argmax(tf.nn.softmax(pooled), axis=1), tf.int32)
         accuracy = tf.reduce_mean(tf.cast(tf.equal(logits_class_num, labels), dtype=tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
 
-    return graph, input_image, labels, in_training, learning_rate, loss, accuracy, optimizer
+        summaries = tf.summary.merge_all()
 
-def run(iterations, minibatch_size):
+    return graph, input_image, labels, in_training, learning_rate, loss, accuracy, summaries, optimizer
+
+def run(iterations, minibatch_size, run_number):
     # ImageNet
     # input_height = input_width = 227
     # input_channels = 3
@@ -167,12 +193,13 @@ def run(iterations, minibatch_size):
 
     train_samples = x_train.shape[0]
 
-    graph, input_batch, labels, in_training, learning_rate, loss, accuracy, optimizer = \
+    graph, input_batch, labels, in_training, learning_rate, loss, accuracy, summaries, optimizer = \
         model(input_height, input_width, input_channels, output_classes, (1, 2, 2, 1))
 
     with tf.Session(graph=graph) as sess:
         sess.run(tf.global_variables_initializer())
 
+        train_writer = tf.summary.FileWriter('/tmp/squeezenet/{}'.format(run_number), sess.graph)
         for i in range(iterations):
             # pick random minibatch
             mb_start = np.random.randint(0, train_samples - minibatch_size)
@@ -184,16 +211,18 @@ def run(iterations, minibatch_size):
                 input_batch: mb_data,
                 labels: mb_labels,
                 in_training: True,
-                learning_rate: 0.05
+                learning_rate: 0.0001
             }
 
-            collectibles = [loss, accuracy, optimizer]
+            collectibles = [loss, accuracy, summaries, optimizer]
 
-            loss_val, accurracy_val, _ = sess.run(collectibles, feed_dict=feed_dict)
+            loss_val, accurracy_val, s, _ = sess.run(collectibles, feed_dict=feed_dict)
+
+            train_writer.add_summary(s, i)
             if i % 100 == 0:
                 print('Iteration: {}\t, loss: {:.3f}\t, accuracy: {:.3f}'.format(i, loss_val, accurracy_val))
 
-run(2000, 128)
+run(2000, 128, 5)
 
 
 # define session
